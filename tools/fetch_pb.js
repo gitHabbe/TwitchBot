b2506e3f00fb2d40d8f91f906d1a8c6d8e706209
githabbe <91habbe@gmail.com>
2020-01-19 01:36:38 +0100

    cleaned PB unused vars

--- tools/fetch_pb.js
+++ tools/fetch_pb.js
@@ -1,20 +1,22 @@
-const util = require('./util.js');
-const fetching = require('./fetching.js');
+const util = require("./util.js");
+const fetching = require("./fetching.js");
 
 async function fetch_pb(info_object) {
-    let { channel, userstate, message, split_msg, game_id, category_id, fuse_hit, speedrun_game, runner } = info_object;
+    let { channel, category_id, fuse_hit } = info_object;
     let speedrunner = await fetching.get_speedrunner(channel);
-    speedrunner = speedrunner.data.data.find(runner => runner.names.international.toLowerCase() === channel.toLowerCase())
-    
-    const pbs_uri = util.get_game_link(speedrunner, 'personal-bests');
+    speedrunner = speedrunner.data.data.find(
+        runner => runner.names.international.toLowerCase() === channel.toLowerCase()
+    );
+
+    const pbs_uri = util.get_game_link(speedrunner, "personal-bests");
     const speedrunners_pbs = await fetching.fetch_speedrun_uri(pbs_uri);
     const the_run = speedrunners_pbs.data.data.find(sub => sub.run.category === category_id);
     const pb_time = util.secondsToString2(the_run.run.times.primary_t);
-    const days_ago = Math.floor((new Date() - new Date(the_run.run.date)) / 86400000)
+    const days_ago = Math.floor((new Date() - new Date(the_run.run.date)) / 86400000);
 
-    return `${channel}'s ${fuse_hit.category} PB: ${pb_time}, ${days_ago} days ago`
+    return `${channel}'s ${fuse_hit.category} PB: ${pb_time}, ${days_ago} days ago`;
 }
 
 module.exports = {
-    fetch_pb: fetch_pb
-}
+    fetch_pb
+};
