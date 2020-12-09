cc6328fbcd96bfd530fda8baaad2eee9da3b15db
HabbeDesktop <91habbe@gmail.com>
2020-12-09 13:06:34 +0100

    nothing

---
+++ options.js
@@ -0,0 +1,18 @@
+// Array of channels to join
+const channels = require("./private/channels.json");
+
+module.exports = {
+    options: {
+        clientId: process.env.TMI_CLIENT,
+        debug: true
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: process.env.TMI_USERNAME,
+        password: "oauth:" + process.env.TMI_TOKEN
+    },
+    channels: channels
+};
---
+++ tools/db.js
@@ -0,0 +1,89 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const cool = {};
+
+cool.getGameByAbbrev = abbrev => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    const gameDB = db.get("games");
+    const is_gameDB = gameDB.find({ abbrev }).value();
+
+    return is_gameDB;
+};
+
+cool.getGameById = id => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    const gameDB = db.get("games");
+    const is_gameDB = gameDB.find({ id }).value();
+
+    return is_gameDB;
+};
+
+cool.saveGame = (speedrun_game, category_list, userstate) => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    const categories = category_list.map(cate => ({ id: cate.id, name: cate.name }));
+    const game_obj = {
+        id: speedrun_game.data.data[0].id,
+        abbrev: speedrun_game.data.data[0].abbreviation,
+        categories,
+        added_by: userstate.username,
+        date: new Date()
+    };
+
+    db.get("games")
+        .push(game_obj)
+        .write();
+};
+
+cool.getGameCategories = game_id => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    this.getGameById(game_id);
+};
+
+cool.getSpeedrunnerAlias = name => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    let speedrunner = db
+        .get("users")
+        .find({ name })
+        .get("settings.srcName")
+        .value();
+    if (speedrunner) return speedrunner;
+    return name;
+};
+
+cool.isRunnerLocal = (info_object, name) => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    speedrunner = db
+        .get("runners")
+        .find({ name: name })
+        .value();
+    if (speedrunner) {
+        info_object.runner = speedrunner;
+        return true;
+    }
+    return false;
+};
+
+cool.getRunnerLocal = name => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    return db
+        .get("runners")
+        .find({ name: name })
+        .value();
+};
+
+cool.runnerToDB = (name, id) => {
+    const adapter = new FileSync("./private/database.json");
+    const db = low(adapter);
+    db.get("runners")
+        .push({ name, id })
+        .write();
+};
+
+module.exports = cool;
